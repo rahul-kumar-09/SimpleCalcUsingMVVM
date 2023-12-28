ef7790f81f0133d920d0dcf365d4183a0d7ca3e8
Rahul kumar maury <rahu7827759@gmail.com>
2023-12-28 22:11:26 +0530

    Sum of two number using mvvm.

---
+++ app/src/main/java/com/programmingz/calcusingmvvm/view/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.programmingz.calcusingmvvm.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.programmingz.calcusingmvvm.R
+import com.programmingz.calcusingmvvm.databinding.ActivityMainBinding
+import com.programmingz.calcusingmvvm.viewmodel.CalculatorViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var calculatorViewModel: CalculatorViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        calculatorViewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
+
+        binding.button.setOnClickListener {
+            val num1 = binding.etNum1.text.toString().toIntOrNull() ?: 0
+            val num2 = binding.etNum2.text.toString().toIntOrNull() ?: 0
+
+            val result = calculatorViewModel.calculateSum(num1, num2)
+            binding.txtResult.text = "${result.sum}"
+
+        }
+
+    }
+}
